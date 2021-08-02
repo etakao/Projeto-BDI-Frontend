b4fb4c620f0bf9270abcb60eccfb76581de63393
ga-sr <gabriel.10rocha@hotmail.com>
2021-08-02 17:57:01 -0300

    add Forms

--- src/pages/Forms/index.js
+++ src/pages/Forms/index.js
@@ -4,10 +4,35 @@ import './styles.scss';
 
 export function Forms() {
   return (
+    <div className="container">
     <div id="header">
       <h2>Cadastro no sistema Covid em Foco</h2>
       <p>Preencha o formulário e contribua com a nossa pesquisa :D</p>
     </div>
-
+    <div className="forms">
+      <form onSubmit={()=> ("a")}>
+        <span>Nome</span>
+        <input type="text" />
+        <span>CPF</span>
+        <input type="text" />
+        <span>Data de nascimento</span>
+        <input type="date" />
+        <span>Período de infecção</span>
+        <div className="infectionPeriod">
+          <span>De</span>
+          <input type="date" />
+          <span>à</span>
+          <input type="date" />
+        </div>
+        <span>CEP</span>
+        <input type="text" />
+        <div className="checkboxClass">
+          <input type="checkbox" />
+          <span>Compreendo e aceito com os </span><a href="/forms">Termos e Condições do site</a>
+        </div>
+        <button>Cadastrar</button>
+      </form>
+    </div>
+    </div>
   );
 }
