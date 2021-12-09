1965262451f5fdb84500b1dfbf9417d96b9d1df2
Erick <ericktakao28@gmail.com>
2021-12-09 15:51:16 -0300

    feat: adicionando novas funcionalidades

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,15 +1,42 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
 import { FiExternalLink, FiMenu } from 'react-icons/fi';
+import Geocode from 'react-geocode';
 
 import { CreateCaption } from './captions.js';
+
+import { cities } from '../../database';
 
 import 'leaflet/dist/leaflet.css';
 import './styles.scss';
 
 export function Maps() {
-  const [selectedCity, setSelectedCity] = useState('presidente-prudente');
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [activeCep, setActiveCep] = useState("");
+  const [activeCoord, setActiveCoord] = useState("");
+
+  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_TOKEN);
+  Geocode.setLanguage("pt-br");
+  Geocode.setRegion("br");
+
+  async function convertCepToCoord(cep) {
+    if (cep !== "") {
+      const response = await Geocode.fromAddress(cep);
+      if (response.status === "OK") {
+        setActiveCoord(response.results[0].geometry.location);
+      }
+    }
+  };
+
+  function changeSelectedCity(cep) {
+    setSelectedCity(cities.find(city => city.cep === cep));
+  };
+
+  useEffect(() => {
+    changeSelectedCity(activeCep);
+    convertCepToCoord(activeCep);
+  }, [activeCep]);
 
   return (
     <div id="maps">
@@ -21,8 +48,30 @@ export function Maps() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        {/* {cities.map(city => {
+          return (
+            <Marker
+              icon={mapIcon}
+              position={[city.lat, city.lon]}
+              key={city.cep}
+            >
+              <Circle closeButton={false} minWidth={240} maxWidth={240}  >
+                {city.name}
+              </Circle>
+            </Marker>
+          );
+        })} */}
+
+        {activeCoord &&
+          <CircleMarker
+            center={[activeCoord.lat, activeCoord.lng]}
+            radius={20}
+          >
+            <Popup>{selectedCity.name}</Popup>
+          </CircleMarker>}
       </MapContainer>
-     
+
       <aside className={isSidebarVisible ? "" : "invisible-sidebar"}>
         <header>
           <FiMenu onClick={() => setIsSidebarVisible(false)} />
@@ -34,16 +83,18 @@ export function Maps() {
             <label htmlFor="select-city">Cidade</label>
             <select
               id="select-city"
-              value={selectedCity}
-              onChange={e => setSelectedCity(e.target.value)}
+              value={activeCep}
+              onChange={e => setActiveCep(e.target.value)}
             >
-              <option value="alvares-machado">Álvares Machado</option>
-              <option value="presidente-prudente">Presidente Prudente</option>
+              <option value="" disabled selected>Selecione uma cidade</option>
+              {cities.map(city => (
+                <option key={city.cep} value={city.cep}>{city.name}</option>
+              ))}
             </select>
           </div>
 
           <div className="city-info">
-            <h2>Presidente Prudente</h2>
+            <h2>{activeCep ? selectedCity.name : "Cidade"}</h2>
 
             <hr />
 
@@ -71,7 +122,7 @@ export function Maps() {
       <div className={isSidebarVisible ? "invisible-toggle" : "toggle-sidebar"} onClick={() => setIsSidebarVisible(true)}>
         <FiMenu />
       </div>
-      
+
       <CreateCaption />
     </div>
   );
