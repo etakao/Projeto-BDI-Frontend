56c588f5d00fbfd2ef12446344f0b1d7adc707c7
Erick <ericktakao28@gmail.com>
2022-01-25 14:47:59 -0300

    feat: change Cadastro's page

--- src/pages/Forms/index.js
+++ src/pages/Forms/index.js
@@ -1,55 +1,154 @@
-import React, { useState } from 'react';
-import Geocode from 'react-geocode';
+import React, { useEffect, useState } from "react";
 
-import './styles.scss';
+import "./styles.scss";
 
 export function Forms() {
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [birthDate, setBirthDate] = useState("");
   const [cep, setCep] = useState("");
+  const [doses, setDoses] = useState("");
+  const [dosesDate, setDosesDate] = useState([
+    {
+      description: "1ª dose:",
+      date: ""
+    },
+    {
+      description: "2ª dose:",
+      date: ""
+    },
+    {
+      description: "3ª dose:",
+      date: ""
+    },
+  ]);
+  const [infectionPeriod, setInfectionPeriod] = useState({
+    from: "",
+    to: ""
+  });
+  const [death, setDeath] = useState("");
 
-  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_TOKEN);
-  Geocode.setLanguage("pt-br");
-  Geocode.setRegion("br");
 
-  async function convertCepToCoord() {
-    if (cep !== "") {
-      const response = await Geocode.fromAddress(cep);
-      if (response.status === "OK") {
-        console.log(response.results[0].geometry.location);
-      }
-    }
-  };
+  function handleDoses(index, date) {
+    const newDosesDate = [...dosesDate];
+    newDosesDate[index].date = date;
+    setDosesDate(newDosesDate);
+  }
+
+  function handlePeriod(flag, date) {
+    setInfectionPeriod({ ...infectionPeriod, [flag]: date });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(name, cpf, birthDate, cep, doses, dosesDate, infectionPeriod, death);
+  }
 
   return (
     <div className="container">
       <div id="header">
-        <h2>Cadastro no sistema Covid em Foco</h2>
-        <p>Preencha o formulário e contribua com a nossa pesquisa :D</p>
+        <h2>Cadastro de morador | Covid em Foco</h2>
       </div>
-      <div className="forms">
-        <form onSubmit={() => ("a")}>
-          <span>Nome</span>
-          <input type="text" />
-          <span>CPF</span>
-          <input type="text" />
-          <span>Data de nascimento</span>
-          <input type="date" />
-          <span>Período de infecção</span>
-          <div className="infectionPeriod">
-            <span>De</span>
-            <input type="date" />
-            <span>à</span>
-            <input type="date" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Nome</label>
+        <input
+          type="text"
+          id="name"
+          onChange={e => setName(e.target.value)}
+        />
+
+        <div className="double-column">
+          <div className="column">
+            <label htmlFor="cpf">CPF</label>
+            <input
+              type="text"
+              id="cpf"
+              onChange={e => setCpf(e.target.value)}
+            />
           </div>
-          <span>CEP</span>
-          <input type="text" value={cep} onChange={e => setCep(e.target.value)} />
-          <button type="button" onClick={convertCepToCoord}>Converter</button>
-          <div className="checkboxClass">
-            <input type="checkbox" />
-            <span>Compreendo e aceito com os </span><a href="/forms">Termos e Condições do site</a>
+
+          <div className="column">
+            <label htmlFor="birth-date">Data de nascimento</label>
+            <input
+              type="date"
+              id="birth-date"
+              onChange={e => setBirthDate(e.target.value)}
+            />
           </div>
-          <button>Cadastrar</button>
-        </form>
-      </div>
+        </div>
+
+        <label htmlFor="cep">CEP</label>
+        <input
+          type="text"
+          id="cep"
+          onChange={e => setCep(e.target.value)}
+        />
+
+        <label htmlFor="doses">N° de doses</label>
+        <div className="doses-section">
+          <select id="doses" onChange={e => setDoses(e.target.value)}>
+            <option value={0}>0</option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+          </select>
+          <div className="doses-date">
+            {dosesDate.slice(0, doses).map((dose, index) => (
+              <div key={index} className="choose-date">
+                <label htmlFor={dose.description}>{dose.description}</label>
+                <input
+                  type="date"
+                  id={dose.description}
+                  onChange={e => handleDoses(index, e.target.value)}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <label htmlFor="time">Período de infecção</label>
+        <div className="period-section">
+          <span>De</span>
+          <input type="date" onChange={e => handlePeriod("from", e.target.value)} />
+          <span>à</span>
+          <input type="date" onChange={e => handlePeriod("to", e.target.value)} />
+        </div>
+
+        <label htmlFor="death">Óbito</label>
+        <div className="death-section">
+          <div>
+            <input
+              type="radio"
+              id="opt1"
+              name="obito"
+              value={1}
+              onChange={e => setDeath(e.target.value)}
+            />
+            <label htmlFor="opt1">Sim</label>
+            <input
+              type="radio"
+              id="opt2"
+              name="obito"
+              value={0}
+              onChange={e => setDeath(e.target.value)}
+            />
+            <label htmlFor="opt2">Não</label>
+          </div>
+          {death === "1" ? (
+            <div>
+              <label htmlFor="death-date">Data:</label>
+              <input type="date" />
+            </div>
+          ) : ""}
+        </div>
+
+        <div className="checkboxClass">
+          <input type="checkbox" />
+          <span>Compreendo e aceito com os </span><a href="/forms">Termos e Condições do site</a>
+        </div>
+        <button type="submit">Cadastrar</button>
+      </form>
     </div>
   );
 }
