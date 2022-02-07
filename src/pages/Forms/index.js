e933a7c8193756920e052d8bee485c9ca81d8ecc
quilantera <gugaquilante@gmail.com>
2022-02-07 20:32:59 -0300

    adicionando as funções do mapa

--- src/pages/Forms/index.js
+++ src/pages/Forms/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Geocode from 'react-geocode';
 
 import './styles.scss';
 
 export function Forms() {
+  const [cep, setCep] = useState("");
+
+  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_TOKEN);
+  Geocode.setLanguage("pt-br");
+  Geocode.setRegion("br");
+
+  async function convertCepToCoord() {
+    if (cep !== "") {
+      const response = await Geocode.fromAddress(cep);
+      if (response.status === "OK") {
+        console.log(response.results[0].geometry.location);
+      }
+    }
+  };
+
   return (
     <div className="container">
       <div id="header">
@@ -10,7 +26,7 @@ export function Forms() {
         <p>Preencha o formulário e contribua com a nossa pesquisa :D</p>
       </div>
       <div className="forms">
-        <form onSubmit={()=> ("a")}>
+        <form onSubmit={() => ("a")}>
           <span>Nome</span>
           <input type="text" />
           <span>CPF</span>
@@ -25,7 +41,8 @@ export function Forms() {
             <input type="date" />
           </div>
           <span>CEP</span>
-          <input type="text" />
+          <input type="text" value={cep} onChange={e => setCep(e.target.value)} />
+          <button type="button" onClick={convertCepToCoord}>Converter</button>
           <div className="checkboxClass">
             <input type="checkbox" />
             <span>Compreendo e aceito com os </span><a href="/forms">Termos e Condições do site</a>
--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,16 +1,171 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
+import { FiExternalLink, FiMenu,FiAlertCircle } from 'react-icons/fi';
+import Geocode from 'react-geocode';
 
-import { FiExternalLink, FiMenu } from 'react-icons/fi';
+import { CreateCaption } from '../../components/Captions/index.js';
+
+import { cities } from '../../database 13-01';
+import { boletim } from '../../boletim';
 
 import 'leaflet/dist/leaflet.css';
 import './styles.scss';
-import './captions.js';
-import { CreateCaption } from './captions.js';
 
 export function Maps() {
-  const [selectedCity, setSelectedCity] = useState('presidente-prudente');
+  /*const atualDate = new Date()
+  const day = String(atualDate.getDate()).padStart(2,'0')
+  const month = String(atualDate.getMonth()+ 1).padStart(2,'0')
+  const year = atualDate.getFullYear()*/
+
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [activeCep, setActiveCep] = useState("");
+  const [activeCoord, setActiveCoord] = useState("");
+  const [reportCard, setReportCard] = useState("");
+  //const [date, setDate]= useState(`${year}-${month}-${day}`)
+  const [date,setDate] = useState('2022-01-24');
+
+  const [citiesCoord, setCitiesCoord] = useState([]);
+  const [citiesReport, setCitiesReport] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_TOKEN);
+  Geocode.setLanguage("pt-br");
+  Geocode.setRegion("br");
+
+  async function convertCepToCoord(cep) {
+    if (cep !== "") {
+      const response = await Geocode.fromAddress(cep);
+      if (response.status === "OK") {
+        setActiveCoord(response.results[0].geometry.location);
+      }
+    }
+  };
+
+  function handleRadiusStatus(status) {
+    switch (status){
+      case 1:
+        return 10;
+      case 2:
+        return 14;
+      case 3:
+        return 18;
+      case 4:
+        return 22;
+      case 5:
+        return 26;
+      default:   
+        return 5;
+    }
+  }
+  
+  function selectedRisk(status){
+    switch (status){
+      case 1:
+        return("#78D1E1")
+      case 2:
+        return("#67E480")
+      case 3:
+        return("#E7DE79")
+      case 4:
+        return("#E89E64")
+      case 5:
+        return("#E96379")
+      default:   
+        return("#78D1E1")
+    }
+  }
+
+  function defaultOption(date){
+    const auxReportCard = boletim.reduce((auxReportCard,RCpos) =>{
+      if(RCpos.data === date){
+        auxReportCard.confirmados = auxReportCard.confirmados + RCpos.confirmados;
+        auxReportCard.total_confirmados= auxReportCard.total_confirmados + RCpos.total_confirmados;
+        auxReportCard.total_obtios = auxReportCard.total_obtios+ RCpos.total_obtios;
+        auxReportCard.total_vacinados = auxReportCard.total_vacinados + RCpos.total_vacinados;
+      }
+      return(auxReportCard)
+    }, { 
+      id: 0,
+      codigo_ibge: '',
+      data: date,
+      confirmados: 0,
+      total_confirmados: 0,
+      total_obtios: 0,
+      total_vacinados: 0,
+      status: 0
+    });
+    setReportCard(auxReportCard);
+  }
+
+  function changeSelectedCity(cep) {
+    setSelectedCity(citiesReport.find(city => city.cep === cep))
+  };
+  
+  async function getCityCoord(cep) {
+    if (cep !== "") {
+      const response = await Geocode.fromAddress(cep);
+      if (response.status === "OK") {
+        return response.results[0].geometry.location;
+      }
+    }
+  }
+
+  useEffect(() => {
+    const promises = [];
+
+    defaultOption(date);
+
+    const filteredCities = boletim.filter(city => 
+      city.data === date
+    );
+    setCitiesReport(filteredCities);
+
+    for (const city of filteredCities) {
+      promises.push(getCityCoord(city.cep));
+    }
+    
+    Promise.all(promises).then(response => {
+      setCitiesCoord(response);
+      setIsLoading(false);
+    });
+
+    // eslint-disable-next-line
+  }, [date]);
+
+  useEffect(() => {
+    changeSelectedCity(activeCep);
+    convertCepToCoord(activeCep);
+
+    // eslint-disable-next-line
+  }, [activeCep]);
+
+  useEffect(() => {
+    if (selectedCity) {
+      console.log(citiesReport)
+      setReportCard(citiesReport.find(city => 
+        city.codigo_ibge === selectedCity.codigo_ibge
+      ));
+    }
+
+    // eslint-disable-next-line
+  }, [selectedCity, date]);
+
+  const putMapMark = () => {
+    if (activeCoord && selectedCity) {
+      return (
+        <CircleMarker
+          center={[activeCoord.lat, activeCoord.lng]}
+          fillOpacity={"80%"}
+          opacity={"60%"}
+          pathOptions={{ color: selectedRisk(reportCard.status) }}
+          radius={handleRadiusStatus(reportCard.status)}
+        >
+          <Popup>{selectedCity.name}</Popup>
+        </CircleMarker>
+      );
+    } 
+  }
 
   return (
     <div id="maps">
@@ -21,55 +176,99 @@ export function Maps() {
       >
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-        />
+        />        
+        
+        {putMapMark()}
+
+        {(!isLoading && (activeCep === "")) &&
+          citiesReport.map((city, index) => {
+            return (
+              <CircleMarker
+                key={city.codigo_ibge}
+                center={[citiesCoord[index].lat, citiesCoord[index].lng]}
+                radius={handleRadiusStatus(city.status)}
+                fillOpacity={"80%"}
+                opacity={"60%"}
+                pathOptions={{ color: selectedRisk(city.status) }}
+              >
+                <Popup>{city.name}</Popup>
+              </CircleMarker>
+            );
+          })
+        }
+         
       </MapContainer>
-      
       <aside className={isSidebarVisible ? "" : "invisible-sidebar"}>
         <header>
           <FiMenu onClick={() => setIsSidebarVisible(false)} />
-          <h1>Covid em foco</h1>
-          
+          <h1>Covid em Foco</h1>
         </header>
-
         <main>
           <div className="city-selection">
             <label htmlFor="select-city">Cidade</label>
             <select
               id="select-city"
-              value={selectedCity}
-              onChange={e => setSelectedCity(e.target.value)}
+              value={activeCep}
+              onChange={e => setActiveCep(e.target.value)}
             >
-              <option value="alvares-machado">Álvares Machado</option>
-              <option value="presidente-prudente">Presidente Prudente</option>
+              <option key={'deafult'} value="" >Selecione uma cidade</option>
+              {cities.map(city => (
+                <option key={city.cep} value={city.cep}>{city.name}</option>
+              ))}
             </select>
           </div>
 
           <div className="city-info">
-            <h2>Presidente Prudente</h2>
-
+            <h2>{activeCep ? "Dados Completos" : "Dados Gerais"}</h2>
             <hr />
-
-            <h3>Dados gerais</h3>
-            <p>Casos:</p>
-            <p>Óbitos:</p>
-            <p>Data do primeiro caso:</p>
-            <p>População estimada:</p>
-
-            <hr />
-
-            <h3>Boletim</h3>
-            <p>Data:</p>
-            <p>Confirmados:</p>
-            <p>Óbitos:</p>
+            <div className="info-container">
+              <p>Total de casos: </p>
+              <p>{reportCard ? reportCard.total_confirmados : 0}</p>
+            </div>
+            <div className="info-container">
+              <p>Total de óbitos: </p>
+              <p>{reportCard ? reportCard.total_obtios : 0}</p>
+            </div>
+            <div className="info-container">
+              <p>Total de vacinados: </p>
+              <p>{reportCard ? reportCard.total_vacinados : 0}</p>
+            </div>
+            <div className="info-container">
+              <p>População estimada: </p>
+              <p>{selectedCity ?  selectedCity.populacao_estimada: 0 }</p>
+            </div>
           </div>
-        </main>
+          <div className="city-info">
+            <h2>Boletim</h2>
+            <hr />
+            <div className="info-container">
+                <p>Data:</p> 
+                <input type='date'key="date" onChange={e => setDate(e.target.value)} value={date}/>
+            </div>
+            <div className="info-container">
+                <p>Confirmados: </p>
+                <p>{reportCard ? reportCard.confirmados : 0}</p>
+            </div>
+            <div className="info-container">
+                <p>Óbitos: </p>
+            </div>
+            <a href="/forms" className='aside-link'>Ver mais Informações <FiAlertCircle/></a>
+          </div>
 
+          <div className="city-info">
+            <h2>Linha do tempo</h2>
+            <hr />
+            <p><input type='checkbox' name="cases" /> Casos</p>
+            <p><input type='checkbox' name="deaths" /> Óbitos</p>
+            <p><input type='checkbox' name="vaccinated" /> Vacinados</p>
+          </div>
+
+        </main>
         <footer>
-          <a href="/forms">Cadastrar novo caso <FiExternalLink /></a>
+          <a href="/forms" className='aside-link'>Cadastrar novo caso <FiExternalLink /></a>
           <span>Direitos reservados</span>
         </footer>
       </aside>
-     
       <div className={isSidebarVisible ? "invisible-toggle" : "toggle-sidebar"} onClick={() => setIsSidebarVisible(true)}>
         <FiMenu />
       </div>
