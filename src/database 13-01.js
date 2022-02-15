70192dd4de423b345cd6626bf9175af76bd49a78
Erick <ericktakao28@gmail.com>
2022-02-15 11:26:25 -0300

    feat: arrruma css e adapta para integracao

--- src/database 13-01.js
+++ src/database 13-01.js
@@ -1,375 +1,375 @@
 export const cities = [
   {
-    "codigo_ibge":'3500105',
-    "name": "Adamantina",
+    "codigo_ibge": '3500105',
+    "nome": "Adamantina",
     "cep": "17800-000",
     "populacao_estimada": 35111,
   },
   {
-    "codigo_ibge":'3500808',
-    "name": "Alfredo Marcondes",
+    "codigo_ibge": '3500808',
+    "nome": "Alfredo Marcondes",
     "cep": "19180-000",
-    "populacao_estimada":4184,
+    "populacao_estimada": 4184,
   },
   {
-    "codigo_ibge":'3501301',
-    "name": "Álvares Machado",
+    "codigo_ibge": 3501301,
+    "nome": "Álvares Machado",
     "cep": "19160-000",
-    "populacao_estimada":24998,
+    "populacao_estimada": 24998,
   },
   {
-    "codigo_ibge":'3502408',
-    "name": "Anhumas",
+    "codigo_ibge": '3502408',
+    "nome": "Anhumas",
     "cep": "19580-000",
-    "populacao_estimada":4144,
+    "populacao_estimada": 4144,
   },
   // {
   //   "codigo_ibge":'3508900',
-  //   "name": "Caiabu",
+  //   "nome": "Caiabu",
   //   "cep": "19530-000",
   //   "populacao_estimada":4193,
   // },
   // {
   //   "codigo_ibge":'3509106',
-  //   "name": "Caiuá",
+  //   "nome": "Caiuá",
   //   "cep": "19450-000",
   //   "populacao_estimada":5946,
   // },
   // {
   //   "codigo_ibge":'3514403',
-  //   "name": "Dracena",
+  //   "nome": "Dracena",
   //   "cep": "17900-000",
   //   "populacao_estimada":47043,
   // },
   // {
   //   "codigo_ibge":'3515129',
-  //   "name": "Emilianópolis",
+  //   "nome": "Emilianópolis",
   //   "cep": "19350-000",
   //   "populacao_estimada":3227,
   // },
   // {
   //   "codigo_ibge":'3515301',
-  //   "name": "Estrela do Norte",
+  //   "nome": "Estrela do Norte",
   //   "cep": "19230-000",
   //   "populacao_estimada":2770,
 
   // },
   // {
   //   "codigo_ibge":'3515350',
-  //   "name": "Euclides da Cunha Paulista",
+  //   "nome": "Euclides da Cunha Paulista",
   //   "cep": "19275-000",
   //   "populacao_estimada":9325,
 
   // },
   // {
   //   "codigo_ibge":'3515806',
-  //   "name": "Flora Rica",
+  //   "nome": "Flora Rica",
   //   "cep": "17870-000",
   //   "populacao_estimada":1430,
 
   // },
   // {
   //   "codigo_ibge":'3516002',
-  //   "name": "Flórida Paulista",
+  //   "nome": "Flórida Paulista",
   //   "cep": "17830-000",
   //   "populacao_estimada":14790,
 
   // },
   // {
   //   "codigo_ibge":'3519907',
-  //   "name": "Iepê",
+  //   "nome": "Iepê",
   //   "cep": "19640-000",
   //   "populacao_estimada":8194,
 
   // },
   // {
   //   "codigo_ibge":'3520608',
-  //   "name": "Indiana",
+  //   "nome": "Indiana",
   //   "cep": "19560-000",
   //   "populacao_estimada":4879,
 
   // },
   // {
   //   "codigo_ibge":'3520806',
-  //   "name": "Inúbia Paulista",
+  //   "nome": "Inúbia Paulista",
   //   "cep": "17760-000",
   //   "populacao_estimada":4019,
 
   // },
   // {
   //   "codigo_ibge":'3521606',
-  //   "name": "Irapuru",
+  //   "nome": "Irapuru",
   //   "cep": "17880-000",
   //   "populacao_estimada":8325,
 
   // },
   // {
   //   "codigo_ibge":'3525607',
-  //   "name": "João Ramalho",
+  //   "nome": "João Ramalho",
   //   "cep": "19680-000",
   //   "populacao_estimada":4551,
 
   // },
   // {
   //   "codigo_ibge":'3526001',
-  //   "name": "Junqueirópolis",
+  //   "nome": "Junqueirópolis",
   //   "cep": "17890-000",
   //   "populacao_estimada":20831,
 
   // },
   // {
   //   "codigo_ibge":'3527405',
-  //   "name": "Lucélia",
+  //   "nome": "Lucélia",
   //   "cep": "17780-000",
   //   "populacao_estimada":21886,
 
   // },
   // {
   //   "codigo_ibge":'3528700',
-  //   "name": "Marabá Paulista",
+  //   "nome": "Marabá Paulista",
   //   "cep": "19430-000",
   //   "populacao_estimada":5948,
 
   // },
   // {
   //   "codigo_ibge":'3528908',
-  //   "name": "Mariápolis",
+  //   "nome": "Mariápolis",
   //   "cep": "17810-000",
   //   "populacao_estimada":4091,
 
   // },
   // {
   //   "codigo_ibge":'3529203',
-  //   "name": "Martinópolis",
+  //   "nome": "Martinópolis",
   //   "cep": "19500-000",
   //   "populacao_estimada":26628,
 
   // },
   // {
   //   "codigo_ibge":'3530201',
-  //   "name": "Mirante do Paranapanema",
+  //   "nome": "Mirante do Paranapanema",
   //   "cep": "19260-000",
   //   "populacao_estimada":18338,
 
   // },
   // {
   //   "codigo_ibge":'3531605',
-  //   "name": "Monte Castelo",
+  //   "nome": "Monte Castelo",
   //   "cep": "17960-000",
   //   "populacao_estimada":4166,
 
   // },
   // {
   //   "codigo_ibge":'3532157',
-  //   "name": "Nantes",
+  //   "nome": "Nantes",
   //   "cep": "19645-000",
   //   "populacao_estimada":3179,
 
   // },
   // {
   //   "codigo_ibge":'3532207',
-  //   "name": "Narandiba",
+  //   "nome": "Narandiba",
   //   "cep": "19220-000",
   //   "populacao_estimada":4904,
 
   // },
   // {
   //   "codigo_ibge":'3533106',
-  //   "name": "Nova Guataporanga",
+  //   "nome": "Nova Guataporanga",
   //   "cep": "17950-000",
   //   "populacao_estimada":2325,
 
   // },
   // {
   //   "codigo_ibge":'3534609',
-  //   "name": "Osvaldo Cruz",
+  //   "nome": "Osvaldo Cruz",
   //   "cep": "17700-000",
   //   "populacao_estimada":33000,
 
   // },
   // {
   //   "codigo_ibge":'3534807',
-  //   "name": "Ouro Verde",
+  //   "nome": "Ouro Verde",
   //   "cep": "17920-000 ",
   //   "populacao_estimada":8620,
 
   // },
   // {
   //   "codigo_ibge":'3534906',
-  //   "name": "Pacaembu",
+  //   "nome": "Pacaembu",
   //   "cep": "17860-000",
   //   "populacao_estimada":14263,
 
   // },
   // {
   //   "codigo_ibge":'3535408',
-  //   "name": "Panorama",
+  //   "nome": "Panorama",
   //   "cep": "17980-000",
   //   "populacao_estimada":15862,
   // },
   // {
   //   "codigo_ibge":'3536406',
-  //   "name": "Paulicéia",
+  //   "nome": "Paulicéia",
   //   "cep": "17990-000",
   //   "populacao_estimada":7454,
 
   // },
   // {
   //   "codigo_ibge":'3538303',
-  //   "name": "Piquerobi",
+  //   "nome": "Piquerobi",
   //   "cep": "19410-000",
   //   "populacao_estimada":3699,
 
   // },
   // {
   //   "codigo_ibge":'3539202',
-  //   "name": "Pirapozinho",
+  //   "nome": "Pirapozinho",
   //   "cep": "19200-000",
   //   "populacao_estimada":27754,
 
   // },
   // {
   //   "codigo_ibge":'3540853',
-  //   "name": "Pracinha",
+  //   "nome": "Pracinha",
   //   "cep": "17790-000",
   //   "populacao_estimada":4212,
 
   // },
   // {
   //   "codigo_ibge":'3541208',
-  //   "name": "Presidente Bernardes",
+  //   "nome": "Presidente Bernardes",
   //   "cep": "19300-000",
   //   "populacao_estimada":13023,
 
   // },
   // {
   //   "codigo_ibge":'3541307',
-  //   "name": "Presidente Epitácio",
+  //   "nome": "Presidente Epitácio",
   //   "cep": "19470-000",
   //   "populacao_estimada":44389,
 
   // },
   // {
   //   "codigo_ibge":'3541406',
-  //   "name": "Presidente Prudente",
+  //   "nome": "Presidente Prudente",
   //   "cep": "19010-081",
   //   "populacao_estimada":230371,
   // },
   // {
   //   "codigo_ibge":'3541505',
-  //   "name": "Presidente Venceslau",
+  //   "nome": "Presidente Venceslau",
   //   "cep": "19400-000",
   //   "populacao_estimada":39583,
 
   // },
   // {
   //   "codigo_ibge":'3541703',
-  //   "name": "Quatá",
+  //   "nome": "Quatá",
   //   "cep": "19780-000",
   //   "populacao_estimada":14210,
 
   // },
   // {
   //   "codigo_ibge":'3542206',
-  //   "name": "Rancharia",
+  //   "nome": "Rancharia",
   //   "cep": "19600-000",
   //   "populacao_estimada":29726,
 
   // },
   // {
   //   "codigo_ibge":'3542404',
-  //   "name": "Regente Feijó",
+  //   "nome": "Regente Feijó",
   //   "cep": "19570-000",
   //   "populacao_estimada":20394,
 
   // },
   // {
   //   "codigo_ibge":'3543238',
-  //   "name": "Ribeirão dos Índios",
+  //   "nome": "Ribeirão dos Índios",
   //   "cep": "19380-000",
   //   "populacao_estimada":2224,
 
   // },
   // {
   //   "codigo_ibge":'3544251',
-  //   "name": "Rosana",
+  //   "nome": "Rosana",
   //   "cep": "19273-000",
   //   "populacao_estimada":16281,
 
   // },
   // {
   //   "codigo_ibge":'3544707',
-  //   "name": "Sagres",
+  //   "nome": "Sagres",
   //   "cep": "17710-000",
   //   "populacao_estimada":2430,
 
   // },
   // {
   //   "codigo_ibge":'3545100',
-  //   "name": "Salmourão",
+  //   "nome": "Salmourão",
   //   "cep": "17720-000",
   //   "populacao_estimada":5337,
 
   // },
   // {
   //   "codigo_ibge":'3545506',
-  //   "name": "Sandovalina",
+  //   "nome": "Sandovalina",
   //   "cep": "19250-000",
   //   "populacao_estimada":4354,
 
   // },
   // {
   //   "codigo_ibge":'3547106',
-  //   "name": "Santa Mercedes",
+  //   "nome": "Santa Mercedes",
   //   "cep": "17940-000",
   //   "populacao_estimada":2943,
 
   // },
   // {
   //   "codigo_ibge":'3547700',
-  //   "name": "Santo Anastácio",
+  //   "nome": "Santo Anastácio",
   //   "cep": "19360-000",
   //   "populacao_estimada":20866,
 
   // },
   // {
   //   "codigo_ibge":'3548302',
-  //   "name": "Santo Expedito",
+  //   "nome": "Santo Expedito",
   //   "cep": "19190-000",
   //   "populacao_estimada":3135,
 
   // },
   // {
   //   "codigo_ibge":'3549300',
-  //   "name": "São João do Pau d'Alho",
+  //   "nome": "São João do Pau d'Alho",
   //   "cep": "17970-000",
   //   "populacao_estimada":2100,
 
   // },
   // {
   //   "codigo_ibge":'3552908',
-  //   "name": "Taciba",
+  //   "nome": "Taciba",
   //   "cep": "19590-000",
   //   "populacao_estimada":6329,
 
   // },
   // {
   //   "codigo_ibge":'3553906',
-  //   "name": "Tarabai",
+  //   "nome": "Tarabai",
   //   "cep": "19210-000",
   //   "populacao_estimada":7540,
 
   // },
   // {
   //   "codigo_ibge":'3554300',
-  //   "name": "Teodoro Sampaio",
+  //   "nome": "Teodoro Sampaio",
   //   "cep": "19280-000",
   //   "populacao_estimada":23273,
 
   // },
   // {
   //   "codigo_ibge":'3555109',
-  //   "name": "Tupi Paulista",
+  //   "nome": "Tupi Paulista",
   //   "cep": "17930-000",
   //   "populacao_estimada":15583,
 
